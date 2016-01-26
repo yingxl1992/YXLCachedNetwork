874cea5956ab049fa813e40d3de70e3214c8733e
yingxiaoli <yingxl1992@163.com>
2016-01-26 15:00:53 +0800

    创建工程

---
+++ YXLCachedNetwork/YXLCachedNetwork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXLCachedNetwork
+//
+//  Created by yingxl1992 on 16/1/26.
+//  Copyright © 2016年 yingxl1992. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YXLCachedNetwork/YXLCachedNetwork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YXLCachedNetwork
+//
+//  Created by yingxl1992 on 16/1/26.
+//  Copyright © 2016年 yingxl1992. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
